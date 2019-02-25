cd5697c7517507d5242d949bb1f7b84add45320a
miohsu <kunieshow@163.com>
2019-02-25 13:17:01 +0800

    pathdownloader

--- lib/download.py
+++ lib/download.py
@@ -14,19 +14,29 @@ class Downloader(object):
 
 
 class PathDownloader(Downloader):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.storge_path = os.path.join(self.storge_path, self.url.rstrip('/').split('/')[-1])
+
     def download(self):
         # download from path
         html = requests.get(self.url).content.decode('utf8')
         analysis = Analysis(html)
         filenames = analysis.get_a_href()
-        storge_path = os.path.join(self.storge_path, self.url.rstrip('/').split('/')[-1])
-        if not os.path.exists(storge_path):
-            os.mkdir(storge_path)
+
+        try:
+            if not os.path.exists(self.storge_path):
+                os.mkdir(self.storge_path)
+            elif not os.path.isdir(self.storge_path):
+                raise NameError("Error: '{}' is exists, but is not a dir.".format(self.storge_path))
+        except NameError as e:
+            print(e)
+            return
 
         for file in filenames:
             url_file = '/'.join([self.url.rstrip('/'), file])
             response = requests.get(url_file, stream=True)
-            with open(os.path.join(storge_path, file), 'wb') as fp:
+            with open(os.path.join(self.storge_path, file), 'wb') as fp:
                 for chunk in response.iter_content(1024):
                     if chunk:
                         fp.write(chunk)
@@ -53,7 +63,6 @@ if __name__ == '__main__':
     # fdl.download()
 
     url_path = 'https://mirrors.aliyun.com/centos/dostools/'
-
 
     pdl = PathDownloader(url_path, storge_path)
     pdl.download()
