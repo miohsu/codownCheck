870e65835cf5118cf32fbad66f2e5326b338cae1
miohsu <kunieshow@163.com>
2019-05-16 00:05:41 +0800

    516

--- conf/config.py
+++ conf/config.py
@@ -2,19 +2,15 @@ import os
 
 BASEDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SVN_ADDRESS = 'https://corp.youdao.com/svn/soft/codown/xue/course-app/apk'
-SVN_ADDRESS = 'http://192.168.1.72/repo'
+SVN_ADDRESS = 'https://corp.youdao.com/svn/soft/codown/xue/course-app/apk'
 
-# CODOWN_ADDRESS = 'http://codown.youdao.com/xue/course-app/apk'
-CODOWN_ADDRESS = 'http://192.168.1.72/repo'
+CODOWN_ADDRESS = 'http://codown.youdao.com/xue/course-app/apk'
 
 SVN_PATH = os.path.join(BASEDIR, 'files', 'svn')
 CODOWN_PATH = os.path.join(BASEDIR, 'files', 'codown')
 
 VERSION = '3.2.0'
 
-# DOWNLOAD_PATH = [VERSION, 'market']
-DOWNLOAD_PATH = ['images', 'repodata']
+DOWNLOAD_PATH = [VERSION, 'market']
 
-# DOWNLOAD_FILE = ['youdao_course_official', 'banner-course-xuewap']
-DOWNLOAD_FILE = ['EULA', 'GPL']
+DOWNLOAD_FILE = ['youdao_course_official', 'banner-course-xuewap']
